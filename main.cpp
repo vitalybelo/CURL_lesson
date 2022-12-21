fe80b5af0d083841c902bf827ed324a16637b4ae
Vitaly <39009460+vitalybelo@users.noreply.github.com>
2022-12-21 14:55:32 +0300

    check_in

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
 
     cpr::Response response = cpr::Get(cpr::Url("https://httpbin.org/get"));
+    //cpr::Response response = cpr::Get(cpr::Url("https://httpbin.org/headers"));
     cout << "STATUS: " << response.status_code << endl;
     cout << response.text << endl;
 
